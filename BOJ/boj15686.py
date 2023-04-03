9de4b57741650caac47117a8fb6f12810d752b8b
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-04-03 20:30:23 +0900

    [BOJ] 치킨 배달 / 골드 5 / 재귀, 백트래킹
    https://www.acmicpc.net/problem/15686

---
+++ BOJ/boj15686.py
@@ -0,0 +1,45 @@
+def get_dist(r1, c1, r2, c2):
+  return abs(r1 - r2) + abs(c1 - c2)
+
+def solution():
+  result1 = []
+  for h in house:
+    count = []
+
+    for s in store:
+      count.append(get_dist(h[0], h[1], s[0], s[1]))
+
+    result1.append(min(count))
+  result2.append(sum(result1))
+
+def f(idx):
+  if len(store) == m:
+    solution()
+    return
+
+  for i in range(idx, len(chicken)):
+    store.append(chicken[i])
+    f(i + 1)
+    store.pop()
+
+n, m = map(int, input().split())
+city = []
+for _ in range(n):
+  city.append(list(map(int, input().split())))
+
+chicken = []
+house = []
+for i in range(n):
+  for j in range(n):
+    if city[i][j] == 2:
+      chicken.append((i, j))
+    elif city[i][j] == 1:
+      house.append((i, j))
+
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+store = []
+result2 = []
+f(0)
+print(min(result2))
