310694f824a37c8e754252cb84c6bbcc8ce12549
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-07-23 20:36:21 +0900

    [BOJ] 후위 표기식 / 골드2 / 스택
    - https://www.acmicpc.net/problem/1918

--- BOJ/boj1918.py
+++ BOJ/boj1918.py
@@ -1,5 +1,5 @@
 string = list(input())
-stack = []
+cmd = []
 result = ''
 
 for s in string:
@@ -7,21 +7,27 @@ for s in string:
     result += s
   else:
     if s == '(':
-      stack.append(s)
+      cmd.append(s)
     elif s == '*' or s == '/':
-      while stack and (stack[-1] == '*' or stack[-1] == '/'):
-        result += stack.pop()
-      stack.append(s)
+      # 앞에서 * 또는 /이 나왔으면 빼낸다.
+      while cmd and (cmd[-1] == '*' or cmd[-1] == '/'):
+        result += cmd.pop()
+      cmd.append(s)
     elif s == '+' or s == '-':
-      while stack and stack[-1] != '(':
-        result += stack.pop()
-      stack.append(s)
+      # +,-는 우선순위가 가장 낮기 때문에 괄호가 나오는게 아닌 이상 + 또는 -가 나타나면
+      # cmd에 존재하는 수식은 모두 빼낸다.
+      while cmd and cmd[-1] != '(':
+        result += cmd.pop()
+      cmd.append(s)
     elif s == ')':
-      while stack and stack[-1] != '(':
-        result += stack.pop()
-      stack.pop()
+      while cmd and cmd[-1] != '(':
+        result += cmd.pop()
+      # ( 제거
+      cmd.pop()
 
-while stack:
-  result += stack.pop()
+while cmd:
+  c = cmd.pop()
+  if c != '(':
+    result += c
 
 print(result)
