aba6d500a61583612b5852b90d49a8ced0dabccf
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-07-24 13:27:07 +0900

    [BOJ] 알파벳 / 골드4 / dfs, 백트래킹
    - https://www.acmicpc.net/problem/1987

--- BOJ/boj1987.py
+++ BOJ/boj1987.py
@@ -1,32 +1,33 @@
-# 각각의 경우가 독립적으로 이루어지기 때문에 visited 처리를 하지 않는다.
-def bfs():
-  global count
-  # 같은 상황은 제외시키기 위해 집합 사용한다.
-  q = set([(0, 0, board[0][0])])
+import sys
 
-  while q:
-    # set 에서 pop 함수는 맨 앞의 요소를 삭제(반환)한다.
-    a, b, s = q.pop()
-    count = max(count, len(s))
+input = sys.stdin.readline
 
-    for i in range(4):
-      if a + dr[i] < r and b + dc[i] < c and a + dr[i] >= 0 and b + dc[i] >= 0:
-        nr = a + dr[i]
-        nc = b + dc[i]
-        if board[nr][nc] not in s:
-          q.add((nr, nc, s + board[nr][nc]))
+def dfs(nr, nc, cnt):
+  global result
+  result = max(result, cnt)
+  
+  for i in range(4):
+    if nr + dr[i] >= 0 and nr + dr[i] < r and nc + dc[i] >= 0 and nc + dc[i] < c:
+      next_r = nr + dr[i]
+      next_c = nc + dc[i]
+      if not alphabet[ord(board[next_r][next_c]) - 65]:
+        alphabet[ord(board[next_r][next_c]) - 65] = True
+        dfs(next_r, next_c, cnt + 1)
+        alphabet[ord(board[next_r][next_c]) - 65] = False
 
 r, c = map(int, input().split())
+
 board = []
 for _ in range(r):
   board.append(list(input()))
 
+alphabet = [False] * 26
+alphabet[ord(board[0][0]) - 65] = True
+
 dr = [0, 1, 0, -1]
-dc = [1, 0 , -1, 0]
+dc = [1, 0, -1, 0]
 
-count = 1
+result = 0
+dfs(0, 0, 1)
 
-bfs()
-print(count)
-
-
+print(result)
